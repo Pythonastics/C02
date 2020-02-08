eb4a39583d1efdd707092b710d21e442006ea470
mischievousdev <adevhubyt@gmail.com>
2020-02-08 20:09:07 +0530

    linting

--- .lint.py
+++ .lint.py
@@ -1,10 +1,15 @@
 import sys
-	
+
+from os import listdir
+from os.path import join
 from pylint.lint import Run
 	
 THRESHOLD = 7.50
+
+cogs = [join("cogs", c) for c in listdir("./cogs") if c.endswith(".py")]
+utils = [join("cogs.utls", c) for c in listdir("./cogs/utils") if c.endswith(".py")]
 	
-result = Run(["venus.py"])
+result = Run(["bot.py", *cogs, *utils])
 	
 score = result.linter.stats["global_note"]
 if score <= THRESHOLD:
